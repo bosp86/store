2a71db50d47e2dd0bd412c106bb8552ca2bea9eb
Bruno Salazar <bosp86@gmail.com>
2018-10-08 10:57:33 -0500

    added about us

--- store-frontend/src/AboutUs.js
+++ store-frontend/src/AboutUs.js
@@ -4,8 +4,14 @@ class AboutUs extends React.Component {
   render() {
     return (
       <div className="product-container">
-        <p>About us</p>
+        <div>
+        <p>SUCUTIENDA PERU Nuestra Empresa: es una floreria peruana en internet con amplia experiencia en el envio de plantas decorativas a Peru (más de 40 años de experiencia ). </p>
+        <p>Envios a todo los distritos de Lima y el resto del pais. Envios al extranjero coordinar al numero 990099341.</p>
+        <p>Nuestra especialidad es la suculenta. Que es la suculenta? Las suculentas son plantas capaces de almacenar grandes cantidades de agua entre sus hojas, esto hace que ellas puedan sobrevivir a arduas sequías. El agua la guardan en: las hojas el tallo o las raíces. Así que esta planta es apta para usted por que solo tendrá que acordarse de vez en cuando en regarla.</p>
+        </div>
       </div>
+
+
     );
   }
 }
